9ed65ae10ed817ba8afc1339028e85776681a855
ng4381 <nikitagoloveshko@gmail.com>
2021-06-03 11:04:28 +0300

    030621
    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from "./admin/dashboard/dashboard.component";
+import { AboutComponent } from "./admin/about/about.component";
+import { ProjectsComponent } from "./admin/projects/projects.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "about", component: AboutComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule
+{
+}
